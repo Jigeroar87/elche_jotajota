297995ea3dc55efad3a7144d64b5748b6c7502b3
Jigeroar87 <jimmy.robles.a@gmail.com>
2023-10-28 14:47:07 -0500

    3ero

--- src/Routes/Rutas.jsx
+++ src/Routes/Rutas.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Inicio from '../Pages/Inicio'
-import Productos from '../Pages/Productos'
 import Sobrenosotros from '../Pages/Sobrenosotros'
 import Zonasdereparto from '../Pages/Zonasdereparto'
 import Contactanos from '../Pages/Contactanos'
-import Detalleproductos from '../Pages/Detalleproductos'
-import Categorias  from '../Pages/Categorias'
-import Footer from '../../Components/Footer/Footer'
+import Footer from '../Components/Footer/Footer'
 
-import CartContent from '../../Components/CartContent/CartContent'
+import Navbar from '../Components/Navbar/Navbar'
 
-import Navbar from '../../Components/Navbar/Navbar'
+import DataProvider from "../Components/Context/DataContext";
 
-import DataProvider from "../../Components/Context/DataContext";
-
-import GlobalState from "../../Components/Context/GlobalContext"
+import GlobalState from "../Components/Context/GlobalContext"
 
 const Rutas = () => {
     
@@ -37,13 +32,10 @@ const Rutas = () => {
         <Navbar navbar_brand={navbar_brand}/>
         <Routes>
             <Route path='/' element={<Inicio/>} />
-            <Route path='/Productos' element={<Productos/>} />
             <Route path='/Sobrenosotros' element={<Sobrenosotros/>} />
-            <Route path='/Zonasdereparto' element={<Zonasdereparto/>} />
-            <Route path='/CartContent' element={<CartContent/>} />  
+            <Route path='/Zonasdereparto' element={<Zonasdereparto/>} /> 
             <Route path='/Contactanos' element={<Contactanos/>} />
-            <Route path='/Productos/:id' element={<Detalleproductos/>} />
-            <Route path='/Categorias/:idcategoria' element={<Categorias/>} />
+            
                   
         </Routes>
         <Footer></Footer>
